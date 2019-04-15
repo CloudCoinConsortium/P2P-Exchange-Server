8550fa599ad0f1164f9cb597a2aff9a036d469ee
Navraj Singh <navraj24031981@gmail.com>
2019-04-15 19:55:33 +1000

    Copied PHP files

---
+++ objects/product.php
@@ -0,0 +1,21 @@
+<?php
+class Product{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "products";
+ 
+    // object properties
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $category_id;
+    public $category_name;
+    public $created;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+}
